2701c77e39aecee9eecd06bb68eb12be8fe6de64
sai <unuser@163.com>
2023-04-03 17:20:59 +0800

    增加查询接口

--- backEnd/flask_sqlite.py
+++ backEnd/flask_sqlite.py
@@ -1,21 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from loguru import logger
+import os
 
-db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Z:/WhatForToday/backEnd/dishes.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///backEnd/dishes.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "backEnd", "dishes.db")
+logger.debug(app.config["SQLALCHEMY_DATABASE_URI"])
 app.config["JSON_AS_ASCII"] = False
+db = SQLAlchemy()
 db.init_app(app)
 
 
 class Dishes(db.Model):
-    __tablename__ = "Dishes"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     material = db.Column(db.JSON)
     quantity = db.Column(db.JSON)
     operate = db.Column(db.JSON)
     tips = db.Column(db.JSON)
+
+    def to_json(self):
+        sdict = self.__dict__
+
 
 
 with app.app_context():
@@ -39,12 +46,15 @@ def add_dishes():
     db.session.add(Dishes(material=material, quantity=quantity, operate=operate, tips=tips))
     db.session.commit()
     res = jsonify(material=material, quantity=quantity, operate=operate, tips=tips)
+    logger.debug(res)
     return res
 
 
-@app.route("/query_dishes", methods=['POST'])
-def query_dishes():
+@app.route("/query_material", methods=['POST'])
+def query_dishes_by_material():
     # 查询条件
     material_in_dishes = request.json.get('material_in_dishes')
-    quantity_in_dishes = request.json.get('quantity_in_dishes')
-
+    # quantity_in_dishes = request.json.get('quantity_in_dishes')
+    dishes = db.session.execute(db.select(Dishes).order_by(Dishes.material)).first()
+    logger.debug(str(dishes))
+    return jsonify(dishes)
