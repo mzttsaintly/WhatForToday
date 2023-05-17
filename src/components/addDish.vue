f7633d222b3e0cc672fa402caca22c2c4d0bd6b9
sai <unuser@163.com>
2023-05-17 17:28:35 +0800

    完善布局

--- src/components/addDish.vue
+++ src/components/addDish.vue
@@ -9,7 +9,7 @@ const new_operate = ref("")
 const new_tips = ref("")
 
 const vFocus = {
-  mounted: (el) => el.focus()
+    mounted: (el) => el.focus()
 }
 
 const new_dish = reactive({
@@ -24,34 +24,85 @@ function submit_dish() {
     axios.post(test_url.value, new_dish).then(function (res) {
         console.log(res)
     })
-    
+
 }
 
-const new_material_array = reactive([{data:""}])
-const new_quantity_array = reactive({})
-const new_operate_array = reactive({})
-const new_tips_array = reactive({})
+const new_material_and_quantity_array = reactive([{ material_name: "", material_quantity:"" }])
+const new_operate_array = reactive([{ data: "" }])
+const new_tips_array = reactive([{ data: "" }])
 
 // 材料栏
 function delete_material_input(item) {
-    if (new_material_array.length<=1) {
+    if (new_material_and_quantity_array.length <= 1) {
         return false;
     }
-    new_material_array.splice(new_material_array.indexOf(item), 1)
+    new_material_and_quantity_array.splice(new_material_and_quantity_array.indexOf(item), 1)
 }
 
 function add_material_input() {
-    this.new_material_array.push({data:""})
+    this.new_material_and_quantity_array.push({ material_name: "", material_quantity:"" })
 }
 
 function merge_material() {
+    let msg_m = ""
+    let msg_q = ""
+    for (let m of new_material_and_quantity_array) {
+        msg_m += m.material_name;
+        msg_m += ';'
+        msg_q += m.material_quantity;
+        msg_q += ';'
+    }
+    console.log("material:" + msg_m)
+    console.log("quantity:" + msg_q)
+}
+
+// 操作栏
+function delete_operate_input(item) {
+    if (new_operate_array.length <= 1) {
+        return false;
+    }
+    new_operate_array.splice(new_operate_array.indexOf(item), 1)
+}
+
+function add_operate_input() {
+    this.new_operate_array.push({ data:"" })
+}
+
+function merge_operate_input() {
     let msg = ""
-    for (let m of new_material_array) {
+    for (let m of new_operate_array) {
         msg += m.data;
         msg += ';'
     }
-    console.log(msg)
-    return msg
+    console.log("operate:" + msg)
+}
+
+// tips栏
+function delete_tips_input(item) {
+    if (new_tips_array.length <= 1) {
+        return false;
+    }
+    new_tips_array.splice(new_tips_array.indexOf(item), 1)
+}
+
+function add_tips_input() {
+    this.new_tips_array.push({ data:"" })
+}
+
+function merge_tips_input() {
+    let msg = ""
+    for (let m of new_tips_array) {
+        msg += m.data;
+        msg += ';'
+    }
+    console.log("operate:" + msg)
+}
+
+// 提交
+function post_button() {
+    merge_material()
+    merge_operate_input()
+
 }
 </script>
 
@@ -93,11 +144,56 @@ function merge_material() {
     </form>
 
     <div class="new">
-        <div class="material" v-for="item in new_material_array" :key="item.id">
-            <el-input type="input" placeholder="输入所需材料" v-model="item.data" v-focus></el-input>
-            <el-button type="danger" @click="delete_material_input(item)">-</el-button>
+        <div class="material-div">
+            <div class="kuangkuang" v-for="item in new_material_and_quantity_array" :key="item.id">
+                <el-input type="input" placeholder="输入所需材料" v-model="item.material_name"></el-input>
+                <el-input type="input" placeholder="输入材料数量" v-model="item.material_quantity"></el-input>
+                <el-button type="danger" @click="delete_material_input(item)">-</el-button>
+            </div>
+            <el-button class="add_button" type="primary" @click="add_material_input()">+</el-button>
+            
         </div>
-        <el-button type="primary" @click="add_material_input()">+</el-button>
+
+        <div class="operate-div">
+            <div class="kuangkuang" v-for="item in new_operate_array" :key="item.id">
+                <el-input type="input" placeholder="输入操作步骤" v-model="item.data"></el-input>
+                <el-button type="danger" @click="$event => delete_operate_input(item)">-</el-button>
+            </div>
+            <el-button type="primary" @click="$event => add_operate_input()">+</el-button>
+        </div>
+
+        <div class="tips-div">
+            <div class="kuangkuang" v-for="item in new_tips_array" :key="item.id">
+                <el-input type="input" placeholder="输入注意事项" v-model="item.data"></el-input>
+                <el-button type="danger" @click="$event => delete_tips_input(item)">-</el-button>
+            </div>
+            <el-button type="primary" @click="$event => add_tips_input()">+</el-button>
+        </div>
+
         <el-button type="success" @click="merge_material">提交</el-button>
     </div>
 </template>
+
+<style>
+.new {
+    display: flex;
+    flex-direction: row;
+    border: 2px;
+    
+}
+
+.new.material-div {
+    display: flex;
+    flex-direction: row;
+}
+
+.kuangkuang {
+    display: flex;
+    flex-direction: row;
+    margin: 2px;
+}
+
+.add_button {
+    align-items: center;
+}
+</style>
